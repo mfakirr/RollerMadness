f0bcb896116ee729e524df08e8dddc62c01d518e
mfakir <46379342+mfakirr@users.noreply.github.com>
2021-01-22 14:48:48 +0300

    information
    İt is just have two level.

---
+++ Treasure.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Treasure : MonoBehaviour
+{
+
+	public int value = 10;
+	public GameObject explosionPrefab;
+    private void OnCollisionEnter(Collision collision)
+    {
+		if (collision.gameObject.tag == "Player")
+		{
+			JobDonner();
+		}
+	}
+    void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			JobDonner();
+		}
+	}
+	void JobDonner()
+    {
+		if (GameManager.gm != null)
+		{
+			// tell the game manager to Collect
+			GameManager.gm.Collect(value);
+		}
+
+		// explode if specified
+		if (explosionPrefab != null)
+		{
+			Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+		}
+
+		// destroy after collection
+		Destroy(gameObject);
+	}
+}
